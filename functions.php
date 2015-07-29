42e30620728cf6d7d6108927eff831b9e493100b
Miguel Peixe <miguel@cardume.art.br>
2015-07-29 17:19:28 -0300

    rename funcitons and add gridsystem and normalize

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 /*
  * Clears JEO default front-end styles and scripts
  */
-function jeo_blank_scripts() {
+function infocongo_scripts() {
 
 	// deregister jeo styles
 	wp_deregister_style('jeo-main');
@@ -11,8 +11,12 @@ function jeo_blank_scripts() {
   // deregister jeo site frontend scripts
   wp_deregister_script('jeo-site');
 
+	// register normalize and grid system
+	wp_register_script('infocongo-normalize', get_stylesheet_directory_uri() . '/css/normalize.css', array(), '2.0.4');
+	wp_register_script('infocongo-skeleton', get_stylesheet_directory_uri() . '/css/skeleton.css', array('infocongo-normalize'), '2.0.4');
+
 }
-add_action('wp_enqueue_scripts', 'jeo_blank_scripts', 10);
+add_action('wp_enqueue_scripts', 'infocongo_scripts', 10);
 
 /*
  * JEO Hooks examples
@@ -20,55 +24,55 @@ add_action('wp_enqueue_scripts', 'jeo_blank_scripts', 10);
  */
 
 // Action right after JEO functionality inits
-function jeo_blank_init() {
+function infocongo_init() {
   // Action goes here
 }
-add_action('jeo_init', 'jeo_blank_init');
+add_action('jeo_init', 'infocongo_init');
 
 // Hook scripts after JEO scripts has been initialized
-function jeo_blank_jeo_scripts() {
+function infocongo_jeo_scripts() {
 
   // Register and enqueue scripts here
 
-  wp_enqueue_script('jeo-blank-jeo-scripts', get_stylesheet_directory_uri() . '/js/jeo-scripts.js', array('jquery') , '0.0.1');
+  wp_enqueue_script('infocongo-jeo-scripts', get_stylesheet_directory_uri() . '/js/jeo-scripts.js', array('jquery') , '0.0.1');
 
-  wp_enqueue_script('jeo-blank-styles', get_stylesheet_directory_uri() . '/css/main.css');
+  wp_enqueue_script('infocongo-styles', get_stylesheet_directory_uri() . '/css/main.css', array('infocongo-skeleton'));
 
 }
-add_action('jeo_enqueue_scripts', 'jeo_blank_jeo_scripts', 20);
+add_action('jeo_enqueue_scripts', 'infocongo_jeo_scripts', 20);
 
 // Hook scripts after JEO Marker scripts has been initialized
-function jeo_blank_markers_scripts() {
+function infocongo_markers_scripts() {
 
   // Register and enqueue scripts here
-  wp_enqueue_script('jeo-blank-jeo-markers-scripts', get_stylesheet_directory_uri() . '/js/jeo-markers-scripts.js', array('jquery') , '0.0.1');
+  wp_enqueue_script('infocongo-jeo-markers-scripts', get_stylesheet_directory_uri() . '/js/jeo-markers-scripts.js', array('jquery') , '0.0.1');
 
 }
-add_action('jeo_markers_enqueue_scripts', 'jeo_blank_markers_scripts', 20);
+add_action('jeo_markers_enqueue_scripts', 'infocongo_markers_scripts', 20);
 
 // Filter to change posts GeoJSON data (also changes the GeoJSON API output)
-function jeo_blank_marker_data($data, $post) {
+function infocongo_marker_data($data, $post) {
 
   // Change $data here
 
   return $data;
 }
-add_filter('jeo_marker_data', 'jeo_blank_marker_data', 10, 2);
+add_filter('jeo_marker_data', 'infocongo_marker_data', 10, 2);
 
 // Filter to change GeoJSON response
-function jeo_blank_markers_data($data, $query) {
+function infocongo_markers_data($data, $query) {
 
   // Change $data here
 
   return $data;
 }
-add_filter('jeo_markers_data', 'jeo_blank_markers_data', 10, 2);
+add_filter('jeo_markers_data', 'infocongo_markers_data', 10, 2);
 
 // Filter to programatically change map data
-function jeo_blank_map_data($data, $map) {
+function infocongo_map_data($data, $map) {
 
   // Change $data here
 
   return $data;
 }
-add_filter('jeo_map_data', 'jeo_blank_map_data', 10, 2);
+add_filter('jeo_map_data', 'infocongo_map_data', 10, 2);
